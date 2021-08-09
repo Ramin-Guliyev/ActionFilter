cb9b1fcc971dceb4ce4743ff2cf49906feafe4f1
Ramin-Guliyev <ra1000ishere@outlook.com>
2021-08-09 23:16:55 +0400

    Add project files.

---
+++ ActionFilter/ApiKeySecurity/ApiKeyAuthFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace ActionFilter.ApiKeySecurity
+{
+    public class ApiKeyAuthFilter : IAsyncActionFilter
+    {
+        private const string ApiKeyHeaderName = "Apikey";
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.HttpContext.Request.Headers.TryGetValue(ApiKeyHeaderName, out var potentialApiKey))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            var configuration = context.HttpContext.RequestServices.GetService<IConfiguration>();
+            var apikey = configuration.GetValue<string>("ApiKey");
+            if (!apikey.Equals(potentialApiKey))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            await next();
+        }
+    }
+}
